43248ada56e70809d7f401ec37d0243827ee583c
Yasina Sk <113372256+yasina9119@users.noreply.github.com>
2026-08-07 23:48:28 +0530

    single quantity

---
+++ Day1/ApprovalWorkflow/Program.cs
@@ -0,0 +1,26 @@
+﻿// Approval Workflow: If grand total > ₹1,00,000, print “Manager Approval Required,” else “Auto-Approved.
+
+using System;
+class Approvalworkflow
+{
+    public static void Main()
+    {
+        // purchase order details
+        string item = "steel";
+        int quantity = 1;
+        double unitPrice = 65000;
+
+        // total
+       double total = quantity * unitPrice;
+
+        if(total > 100000)
+        {
+            Console.WriteLine("manage approval is required");
+        }
+        else
+        {
+            Console.WriteLine("Auto approve");
+        }
+
+    }
+}
