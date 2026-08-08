376328215f583ae58f7645eaca718e8e3a79aa07
Yasina Sk <113372256+yasina9119@users.noreply.github.com>
2026-08-09 02:38:55 +0530

    CustomerOrderSimulation

---
+++ Day1/Cloud Integration Simulation/Customer Orders/Program.cs
@@ -0,0 +1,22 @@
+﻿// Order Management Simulation: Store customer order details in a List and print all orders.
+using System;
+using System.Collections.Generic;
+
+class CustomerOrderSimulation
+{
+    public static void Main()
+    {
+        List<string> orders = new List<string>();
+
+        orders.Add("ORD001 - Yasina - Laptop");
+        orders.Add("ORD002 - Happy - Keyboard");
+        orders.Add("ORD003 - Rizwana - Mouse");
+
+        Console.WriteLine("All Orders:");
+
+        foreach (string order in orders)
+        {
+            Console.WriteLine(order);
+        }
+    }
+}
