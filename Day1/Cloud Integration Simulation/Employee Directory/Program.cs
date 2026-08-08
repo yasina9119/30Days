8ae8e635f37208934356511dd3461213e85d3b8a
Yasina Sk <113372256+yasina9119@users.noreply.github.com>
2026-08-09 02:55:04 +0530

    Add employee directory

---
+++ Day1/Cloud Integration Simulation/Employee Directory/Program.cs
@@ -0,0 +1,25 @@
+﻿
+// Employee Directory: Store 5 employee names in a List and print all employee names.using System;
+using System.Collections.Generic;
+
+class EmployeeDirectory
+{
+    public static void Main()
+    {
+        List<string> EmployeeNames = new List<string>();
+
+        EmployeeNames.Add("Bunny");
+        EmployeeNames.Add("cutie");
+        EmployeeNames.Add("chintu");
+        EmployeeNames.Add("kanna");
+        EmployeeNames.Add("sweety");
+
+        Console.WriteLine("EmployeeNames:");
+
+        foreach(string EmployeeName in EmployeeNames)
+        {
+            Console.WriteLine(EmployeeName);
+        }
+
+    }
+}
