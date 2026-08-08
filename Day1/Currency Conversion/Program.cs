84cffe27645f7a3bc464f7c17844967010415437
Yasina Sk <113372256+yasina9119@users.noreply.github.com>
2026-08-09 01:23:40 +0530

    Add currency conversion

---
+++ Day1/Currency Conversion/Program.cs
@@ -0,0 +1,23 @@
+﻿// Currency Conversion: Convert the grand total from INR to GBP using a fixed conversion rate.
+using System;
+class CurrencyConversion
+{
+    public static void Main()
+    {
+        string item = "iron";
+        int Qty = 19;
+        double UnitPrice = 987620;
+        double discount = 10000;
+        double inrToGbp = 0.009;
+
+        double subtotal = Qty * UnitPrice;
+        double tax = subtotal * 0.12;
+        double total = subtotal - discount;
+        double grandTotal = total + tax;
+        double grandTotalGBP = grandTotal * inrToGbp;
+
+        Console.WriteLine($"grand total for the item {item} from INR to GBP using a fixed conversion rate{(inrToGbp)} is : {grandTotalGBP}");
+
+
+    }
+}
