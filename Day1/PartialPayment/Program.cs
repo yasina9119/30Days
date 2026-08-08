f731e235b77ac5c38e6a34af4a2d3029c3cbe700
Yasina Sk <113372256+yasina9119@users.noreply.github.com>
2026-08-09 00:51:23 +0530

    Add partial payment

---
+++ Day1/PartialPayment/Program.cs
@@ -0,0 +1,20 @@
+﻿// Partial Payment: Simulate a case where only 50% of the grand total is paid upfront.
+
+using System;
+class PartialPayment
+{
+    public static void Main()
+    {
+        string item = "metal";
+        int Qty = 5;
+        double UnitPrice = 43250;
+
+       double total = Qty * UnitPrice;
+       double tax = total * 0.18;
+       double grandTotal = total + tax;
+
+       double upfrontPayment = grandTotal * 0.50;
+
+       Console.WriteLine($"Upfront payment: {upfrontPayment}");
+    }
+}
