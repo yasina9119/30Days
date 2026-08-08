41a4af727ab1e43641a6aa2a28504bfd02d81509
Yasina Sk <113372256+yasina9119@users.noreply.github.com>
2026-08-09 00:03:45 +0530

    Add vendor comparison

---
+++ Day1/VendorComparison/Program.cs
@@ -0,0 +1,20 @@
+﻿// Vendor Comparison: Compare two suppliers’ quotes(price) and select the cheaper one.
+
+using System;
+class VendorComparison
+{
+   public static void Main()
+    {
+        int supplier1 = 25000;
+        int supplier2 = 30000;
+
+        if(supplier1 < supplier2)
+        {
+            Console.WriteLine("Select supplier one quote is cheaper");
+        }
+        else
+        {
+            Console.WriteLine("Select supplier2 qoute is cheaper");
+        }
+    }
+}
