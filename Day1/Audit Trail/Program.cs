17db32ab6165f21bdee83a2c2cafcfe59290e76d
Yasina Sk <113372256+yasina9119@users.noreply.github.com>
2026-08-10 02:04:59 +0530

    Audit Trail

---
+++ Day1/Audit Trail/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+
+class AuditTrail
+{
+    public static void Main()
+    {
+        Console.WriteLine($"PO Created: {DateTime.Now}");
+
+        Thread.Sleep(2000);
+
+        Console.WriteLine($"Invoice Generated: {DateTime.Now}");
+
+        Thread.Sleep(2000);
+
+        Console.WriteLine($"Payment Initiated: {DateTime.Now}");
+    }
+}
