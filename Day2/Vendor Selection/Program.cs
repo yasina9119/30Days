dcfe0d7ad8d7c425088b600933ae0e3aa1dbd689
Yasina Sk <113372256+yasina9119@users.noreply.github.com>
2026-08-11 00:38:30 +0530

    Vendor Selection

---
+++ Day2/Vendor Selection/Program.cs
@@ -0,0 +1,19 @@
+﻿// Vendor Selection: If vendor rating ≥ 4 AND delivery time ≤ 7 days → Select vendor; else reject.
+using System;
+class VendorSelection
+{
+    public static void Main()
+    {
+        int rating = 5;
+        int delivery = 4;
+
+        if(rating >= 4 && delivery <= 7)
+        {
+            Console.WriteLine("Select vendor");
+        }
+        else
+        {
+          Console.WriteLine("Reject vendor");   
+        }
+    }
+}
