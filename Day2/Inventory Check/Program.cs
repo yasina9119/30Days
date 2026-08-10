98ef2ae6af13b1c15925627f8dd0b408533b7c80
Yasina Sk <113372256+yasina9119@users.noreply.github.com>
2026-08-11 00:47:00 +0530

    Inventory Check

---
+++ Day2/Inventory Check/Program.cs
@@ -0,0 +1,16 @@
+﻿// Inventory Check: If stock < 50 OR demand > 100 → Trigger replenishment order.
+using System;
+class InventoryCheck
+{
+    public static void Main()
+    {
+        int stock = 43;
+        int demand = 120;
+
+        if(stock < 50 ||  demand > 100)
+        {
+            Console.WriteLine("Trigger replenishment order");
+        }
+    
+    }
+}
