842f32206de812595332c01e9b44c65d949e5816
Yasina Sk <113372256+yasina9119@users.noreply.github.com>
2026-08-11 01:17:31 +0530

    Cloud Integration Simulation

---
+++ Day2/Cloud Integration Simulation/Program.cs
@@ -0,0 +1,19 @@
+﻿// Cloud Integration Simulation: If API connection is live → Push PO data; else retry.
+
+using System;
+class CloudIntegration
+{
+    public static void Main()
+    {
+        bool isLive = false;
+
+        if(isLive == false)
+        {
+            Console.WriteLine("retry");
+        }
+        else
+        {
+         Console.WriteLine("Push PO data");   
+        }
+    }
+}
