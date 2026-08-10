c91d78385d3881c84af7a8d6b0e5f6704e58c10b
Yasina Sk <113372256+yasina9119@users.noreply.github.com>
2026-08-11 02:02:51 +0530

    Multiple PO IDs

---
+++ Day3/multiple PO IDs/Program.cs
@@ -0,0 +1,19 @@
+﻿// Store multiple PO IDs in an array and print which ones are “formalized.”
+using System;
+class MultiplePOIDs
+{
+    public static void Main()
+    {
+        string[] poIds = { "PO101", "PO102", "PO103", "PO104" };
+
+        bool[] formalized = { true, false, true, false };
+
+        for (int i = 0; i < poIds.Length; i++)
+        {
+            if (formalized[i])
+            {
+                Console.WriteLine($"{poIds[i]} is formalized");
+            }
+        }
+    }
+}
