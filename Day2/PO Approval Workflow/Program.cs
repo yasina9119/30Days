a087cd5a93efb88ae8579504d1ce4a4e39456c1e
Yasina Sk <113372256+yasina9119@users.noreply.github.com>
2026-08-11 00:30:18 +0530

    PO Approval Workflow

---
+++ Day2/PO Approval Workflow/Program.cs
@@ -0,0 +1,21 @@
+﻿// PO Approval Workflow: If PO amount > ₹5,00,000, require Director approval; else Manager approval; else auto-approve.
+using System;
+class POApproval
+{
+    public static void Main()
+    {
+        int POAmount = 6500000;
+        if(POAmount > 500000)
+        {
+            Console.WriteLine("require Director approval");
+        }
+        else if(POAmount > 100000)
+        {
+          Console.WriteLine("require Manager approval");   
+        }
+        else
+        {
+            Console.WriteLine("auto-approve");  
+        }
+    }
+}
