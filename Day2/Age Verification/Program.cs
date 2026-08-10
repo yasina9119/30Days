e260a3ca669afa0d7b08a86f59ba9f1ca4754d71
Yasina Sk <113372256+yasina9119@users.noreply.github.com>
2026-08-11 00:12:38 +0530

    Age Verification

---
+++ Day2/Age Verification/Program.cs
@@ -0,0 +1,18 @@
+﻿// Age Verification: Write a program that checks if a user can create a Purchase Requisition (must be ≥18).
+using System;
+class Age
+{
+    public static void Main()
+    {
+        int Age = 24;
+
+        if(Age >= 18)
+        {
+            Console.WriteLine("user can create a Purchase Requisition");
+        }
+        else
+        {
+         Console.WriteLine("user can't create a Purchase Requisition");   
+        }
+    }
+}
