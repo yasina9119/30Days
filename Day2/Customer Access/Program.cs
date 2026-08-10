5cec8fef84e403dbf29f3b21708daa7132d4b016
Yasina Sk <113372256+yasina9119@users.noreply.github.com>
2026-08-11 00:59:00 +0530

    Customer Access

---
+++ Day2/Customer Access/Program.cs
@@ -0,0 +1,20 @@
+﻿// Customer Access: If customer is premium OR order amount > ₹50,000 → Apply discount; else no discount.
+
+using System;
+class CustomerAccess
+{
+    public static void Main()
+    {
+        bool ispremium = true;
+        int orderamount = 65890;
+
+        if(ispremium || orderamount> 50000)
+        {
+            Console.WriteLine("Apply discount");
+        }
+        else
+        {
+            Console.WriteLine("no discount");
+        }
+    }
+}
