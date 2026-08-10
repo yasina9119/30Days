f63e68259dbaf10e1efa3269d76c7b02bce00421
Yasina Sk <113372256+yasina9119@users.noreply.github.com>
2026-08-11 01:09:25 +0530

    Shipment Logic

---
+++ Day2/Shipment Logic/Program.cs
@@ -0,0 +1,17 @@
+﻿// Shipment Logic: If goods received AND payment completed → Mark shipment as “Released.”
+
+using System;
+class ShipmentLogic
+{
+    public static void Main()
+    {
+        bool isgoodsReceived = true;
+        bool ispaymentCompleted = true;
+        string shipment = "Released";
+
+        if(isgoodsReceived && ispaymentCompleted )
+        {
+            Console.WriteLine($"Mark Shipment as {shipment}");
+        }
+    }
+}
