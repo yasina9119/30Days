7567ecdfaca7274f436bd35005998e6fdd79413a
Yasina Sk <113372256+yasina9119@users.noreply.github.com>
2026-08-11 01:35:51 +0530

    Case Escalation

---
+++ Day2/Case Escalation/Program.cs
@@ -0,0 +1,22 @@
+﻿// Case Escalation (CE): If case unresolved > 48 hours → cc else keep in queue.
+
+using System;
+class CaseEscalation
+{
+    public static void Main()
+    {
+        bool isUnresolved = true;
+        double hrs = 55;
+
+        if (isUnresolved && hrs > 48)
+        {
+            Console.WriteLine("Escalate to Manager");
+        }
+        else
+        {
+            Console.WriteLine("Keep in queue");
+        }
+
+
+    }
+}
