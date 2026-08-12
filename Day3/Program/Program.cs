d4b73f79d5b013fa65b327f9d344e952bdb3f835
Yasina Sk <113372256+yasina9119@users.noreply.github.com>
2026-08-13 02:06:22 +0530

    Program

---
+++ Day3/Program/Program.cs
@@ -0,0 +1,20 @@
+﻿// Write a program that checks if PO + Product Receipt + Vendor Invoice all exist → Trigger payment.
+using System;
+class P1
+{
+    public static void Main()
+    {
+        bool isPOPresent = true;
+        bool isPRPresent = true;
+        bool isVIPresent = true;
+
+        if(isPOPresent && isPRPresent && isVIPresent)
+        {
+            Console.WriteLine("Trigger Payment");
+        }
+        else
+        {
+             Console.WriteLine("No Trigger");
+        }
+    }
+}
