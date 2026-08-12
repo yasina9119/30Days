51d610d8dc5ff0258e57c10e175bec1714fbdd33
Yasina Sk <113372256+yasina9119@users.noreply.github.com>
2026-08-13 02:40:48 +0530

    Inventory Updated

---
+++ Day3/Inventory Updated/Program.cs
@@ -0,0 +1,16 @@
+﻿// Build a workflow that prints “Inventory Updated” after each Product Receipt.
+using System;
+
+class ProductReceipt
+{
+    public static void Main()
+    {
+        string[] productReceipts = { "PR001", "PR002", "PR003" };
+
+        for (int i = 0; i < productReceipts.Length; i++)
+        {
+            Console.WriteLine($"Product Receipt: {productReceipts[i]}");
+            Console.WriteLine("Inventory Updated");
+        }
+    }
+}
