c498f96d86f16cfcedde703e849fad851e0cf6f0
Yasina Sk <113372256+yasina9119@users.noreply.github.com>
2026-08-13 01:43:01 +0530

    Goods Receipt

---
+++ Day3/Goods Receipt/Program.cs
@@ -0,0 +1,18 @@
+﻿// Simulate a case where Goods Receipt is missing → Payment blocked.
+using System;
+class GoodsReceipt
+{
+    public static void Main()
+    {
+        bool isGoodsReceipt = false;
+
+        if(isGoodsReceipt)
+        {
+            Console.WriteLine("Payment Successful");
+        }
+        else
+        {
+            Console.WriteLine("Payment blocked");
+        }
+    }
+}
