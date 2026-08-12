c00404a3e952985d1e3f5fdd04e88dc78c8e9bec
Yasina Sk <113372256+yasina9119@users.noreply.github.com>
2026-08-13 02:31:37 +0530

    Stimulate P2P Workflow

---
+++ Day3/StimulateP2PWorkflow/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class P2PWorkflow
+{
+    public static void Main()
+    {
+        bool isRequisition = true;
+        bool isFormalPO = true;
+        bool isProductReceipt = true;
+        bool isVendorInvoice = true;
+
+        if (isRequisition)
+        {
+            Console.WriteLine("Requisition created");
+
+            if (isFormalPO)
+            {
+                Console.WriteLine("Formal PO created");
+
+                if (isProductReceipt)
+                {
+                    Console.WriteLine("Product Receipt completed");
+
+                    if (isVendorInvoice)
+                    {
+                        Console.WriteLine("Vendor Invoice generated");
+
+                        Console.WriteLine("Payment triggered");
+                    }
+                }
+            }
+        }
+    }
+}
