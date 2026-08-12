32c95258c74207e8d00b8c14634813bf28d533ca
Yasina Sk <113372256+yasina9119@users.noreply.github.com>
2026-08-13 01:22:25 +0530

    Multiple Invoices

---
+++ Day3/Multiple Invoices/Program.cs
@@ -0,0 +1,26 @@
+﻿// Store multiple invoice amounts in an array and process only those with matching receipts.
+
+using System;
+class MultipleInvoices
+{
+    public static void Main()
+    {
+        double[] invoiceAmounts = new double[5];
+        invoiceAmounts[0]=8760;
+        invoiceAmounts[1]=4790;
+        invoiceAmounts[2]=5000;
+        invoiceAmounts[3]=1000;
+        invoiceAmounts[4]=2400;
+
+        double [] receipts = {8760,3456,5000,1000,3098};
+
+        for(int i = 0; i< invoiceAmounts.Length; i++)
+        {
+            if(invoiceAmounts[i] == receipts[i])
+            {
+                Console.WriteLine($"Invoice {invoiceAmounts[i]} has a matching receipt");
+            }
+        }
+
+    }
+}
