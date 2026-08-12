2a1bc1e5efca3fcf64a2ab52c813d2fef3b61418
Yasina Sk <113372256+yasina9119@users.noreply.github.com>
2026-08-13 01:54:45 +0530

    payment

---
+++ Day3/payment/Program.cs
@@ -0,0 +1,19 @@
+﻿// Modify the program to block payment if invoice amount > ₹1,00,000 without manager approval.
+using System;
+class Payment
+{
+    public static void Main()
+    {
+        double InvoiceAmount = 250000;
+        bool isapprovalneeded = false;
+
+        if(InvoiceAmount > 100000 && !isapprovalneeded)
+        {
+            Console.WriteLine("Block the payment");
+        }
+        else
+        {
+         Console.WriteLine("UnBlock the payment");   
+        }
+    }
+}
