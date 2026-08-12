961d854a3c600d43a81c4977e507fb6234f1e450
Yasina Sk <113372256+yasina9119@users.noreply.github.com>
2026-08-13 02:15:37 +0530

    Formal PO Created

---
+++ Day3/Formal PO created/Program.cs
@@ -0,0 +1,15 @@
+﻿// Create a program that prints “Formal PO created” only when requisition + approval both exist.
+using System;
+class FormalPOCreated
+{
+    public static void Main()
+    {
+        bool isrequisition = true;
+        bool isapproval = true;
+
+        if(isrequisition && isapproval)
+        {
+            Console.WriteLine("Formal PO Created");
+        }
+    }
+}
