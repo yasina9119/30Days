5a4007cd291cd6ade2ecd8955c7bc96878a98526
Yasina Sk <113372256+yasina9119@users.noreply.github.com>
2026-08-14 01:13:15 +0530

    calculates tax

---
+++ Day4/calculates tax/Program.cs
@@ -0,0 +1,14 @@
+﻿// Build a function that calculates tax on an amount and returns the final invoice total.
+using System;
+class CalculateTotal
+{
+    public static void Main()
+    {
+        static double CalculateTotal(double amount, double taxRate)
+        {
+            double tax = amount * taxRate / 100;
+            return amount + tax;
+        }
+        Console.WriteLine(CalculateTotal(1000, 18));
+    }
+}
