7057931e12930cd3c01461a9bcaa6703e6f74cb7
Yasina Sk <113372256+yasina9119@users.noreply.github.com>
2026-08-14 00:47:49 +0530

    Order Confirmation

---
+++ Day4/OrderConfirmation/Program.cs
@@ -0,0 +1,13 @@
+﻿// Create a function that accepts a customer name and product, and returns an order confirmation string. 
+using System;
+class OrderConfirmation
+{
+    public static void Main()
+    {
+        static string accepts(string customername, string product)
+        {
+            return $"Order confirmed for {customername}: {product}";
+        }
+        Console.WriteLine(accepts("yasina", "co-ord set"));
+    }
+}
