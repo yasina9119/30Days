3c1de186e88a8fcb0c4b62e77d928789dcd208d9
Yasina Sk <113372256+yasina9119@users.noreply.github.com>
2026-08-14 01:05:07 +0530

    Invoice generated

---
+++ Day4/Invoice generated/Program.cs
@@ -0,0 +1,12 @@
+﻿// Write a void function that prints “Invoice generated” without returning anything.
+using System;
+class Invoicegenerated{
+    public static void Main()
+    {
+        static void Invoice()
+        {
+            Console.WriteLine("Invoice Generated");
+        }
+        Invoice();
+    }
+}
