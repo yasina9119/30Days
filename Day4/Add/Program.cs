afe0fc1fdff4a45ac6c85325ebe2e85d8ec64efa
Yasina Sk <113372256+yasina9119@users.noreply.github.com>
2026-08-14 00:35:57 +0530

    Add

---
+++ Day4/Add/Program.cs
@@ -0,0 +1,13 @@
+﻿// Write a function that takes two numbers as parameters and returns their sum.
+using System;
+class Add
+{
+    public static void Main()
+    {
+        static int add (int a, int b)
+        {
+            return a + b;
+        }
+        Console.WriteLine(add(5,8));
+    }
+}
